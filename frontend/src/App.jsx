cc94135b26e5863376269d9dd6f29cc1cf7401bc
Khush Shah <khushshah675@gmail.com>
2025-08-03 01:47:58 +0530

    fix: implement correct React Router v6+ nested routes structure
    - Fix routing structure to use proper nested routes with Outlet
    - Remove direct child components from Layout element prop
    - Implement parent route with ProtectedRoute and Layout
    - Add child routes for all protected pages (Dashboard, Jobs, etc.)
    - This resolves the issue where pages were not rendering due to incorrect route structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,11 @@
 import React, { useEffect } from "react";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate,
+  Outlet,
+} from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { initAuth } from "./store/authSlice";
 import { ThemeProvider } from "./contexts/ThemeContext";
@@ -40,10 +46,8 @@ function ProtectedRoute({ children }) {
 
 function AppContent() {
   const dispatch = useDispatch();
-  const { isAuthenticated } = useSelector((state) => state.auth);
 
   useEffect(() => {
-    console.log("🚀 App: useEffect triggered, dispatching initAuth");
     dispatch(initAuth());
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
@@ -55,137 +59,40 @@ function AppContent() {
         <Route
           path="/login"
           element={
-            isAuthenticated ? (
-              <Navigate to="/dashboard" />
-            ) : (
-              <AuthLayout>
-                <Login />
-              </AuthLayout>
-            )
+            <AuthLayout>
+              <Login />
+            </AuthLayout>
           }
         />
         <Route
           path="/register"
           element={
-            isAuthenticated ? (
-              <Navigate to="/dashboard" />
-            ) : (
-              <AuthLayout>
-                <Register />
-              </AuthLayout>
-            )
+            <AuthLayout>
+              <Register />
+            </AuthLayout>
           }
         />
 
-        {/* Protected Routes */}
+        {/* Protected Routes - NESTED! */}
         <Route
-          path="/"
           element={
             <ProtectedRoute>
-              <Navigate to="/dashboard" />
+              <Layout />
             </ProtectedRoute>
           }
-        />
-        <Route
-          path="/dashboard"
-          element={
-            <ProtectedRoute>
-              <Layout>
-                <Dashboard />
-              </Layout>
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/jobs"
-          element={
-            <ProtectedRoute>
-              <Layout>
-                <Jobs />
-              </Layout>
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/jobs/:id"
-          element={
-            <ProtectedRoute>
-              <Layout>
-                <JobDetail />
-              </Layout>
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/jobs/post"
-          element={
-            <ProtectedRoute>
-              <Layout>
-                <JobPost />
-              </Layout>
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/jobs/applications"
-          element={
-            <ProtectedRoute>
-              <Layout>
-                <Applications />
-              </Layout>
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/applications"
-          element={
-            <ProtectedRoute>
-              <Layout>
-                <Applications />
-              </Layout>
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/feed"
-          element={
-            <ProtectedRoute>
-              <Layout>
-                <Feed />
-              </Layout>
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/profile"
-          element={
-            <ProtectedRoute>
-              <Layout>
-                <Profile />
-              </Layout>
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/connections"
-          element={
-            <ProtectedRoute>
-              <Layout>
-                <Connections />
-              </Layout>
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/settings"
-          element={
-            <ProtectedRoute>
-              <Layout>
-                <Settings />
-              </Layout>
-            </ProtectedRoute>
-          }
-        />
+        >
+          <Route path="/" element={<Navigate to="/dashboard" />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/jobs" element={<Jobs />} />
+          <Route path="/jobs/:id" element={<JobDetail />} />
+          <Route path="/jobs/post" element={<JobPost />} />
+          <Route path="/jobs/applications" element={<Applications />} />
+          <Route path="/applications" element={<Applications />} />
+          <Route path="/feed" element={<Feed />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/connections" element={<Connections />} />
+          <Route path="/settings" element={<Settings />} />
+        </Route>
 
         {/* Catch all route */}
         <Route path="*" element={<Navigate to="/dashboard" />} />
